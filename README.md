c04eac538c408f1078373924da74100b064ee852
EsteMaeLeo <leonel_morales20@hotmail.com>
2026-01-08 19:16:21 -0600

    adding readme

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 
 #### Remember for python follow normal Hierarchy operation: 
 ##### first parentesis, exponentiation, multiplication and division, addition and substraction
+##### Due to some historical quirk, an exception to the left-to-right left-associative rule is the exponentiation operator **. A useful hint is to always use parentheses to force exactly the order you want when exponentiation is involved
--- firststeps.py
+++ firststeps.py
@@ -10,3 +10,23 @@ print(10 ** 5)
 print( (10+20) / 2 )
 print(18.0 // 4)
 print(18 % 4)
+print(2 ** 3 ** 2)     # the right-most ** operator gets done first!
+print((2 ** 3) ** 2)   # use parentheses to force the order you want!
+'''
+Function calls
+'''
+def square(x):
+   return x * x
+
+def sub(x, y):
+   return x - y
+
+print(square(3))
+print(sub(6, 4))
+print(sub(5, 9))
+print(square(3) + 2)
+print(sub(square(3), square(1+1)))
+print(square)
+
+'''Data Types'''
+print("Data Types")
