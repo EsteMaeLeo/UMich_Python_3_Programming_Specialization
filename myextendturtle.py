4bbae7ef84a7782149096b0b8283a6f6a41389b2
EsteMaeLeo <leonel_morales20@hotmail.com>
2026-01-09 00:40:28 -0600

    adding readme

--- myextendturtle.py
+++ myextendturtle.py
@@ -1,17 +1,22 @@
 import turtle
+color = input("Enter Background color ")
+tcolor = input("Enter turtle color ")
+tsize = input("Enter pen size ")
 wn = turtle.Screen()
-wn.bgcolor("black")  # Optional: Set background color
+wn.bgcolor(color)  # Optional: Set background color
 wn.title("Turtle")  # Optional: Window title
 
 # Create a turtle (the drawing "pen")
 my_turtle = turtle.Turtle()
-my_turtle.color("green")  # Pen color
-my_turtle.pensize(6)      # Line thickness
+my_turtle.color(tcolor)  # Pen color
+my_turtle.pensize(int(tsize))      # Line thickness
 
 # Draw a simple square
 for _ in range(4):
     my_turtle.forward(100)  # Move forward 100 pixels
     my_turtle.right(90)     # Turn right 90 degrees
+
+    
 
 # Keep window open until clicked
 wn.exitonclick()
