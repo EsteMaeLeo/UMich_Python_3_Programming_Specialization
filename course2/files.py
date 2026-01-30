efcc0658edc46713d922316798db45cc96c3023b
EsteMaeLeo <leonel_morales20@hotmail.com>
2026-01-30 01:31:56 -0600

    keep journey of learning now files

--- course2/files.py
+++ course2/files.py
@@ -1,3 +1,11 @@
+'''
+open(filename,'r') Open a file called filename and use it for reading. This will return a reference to a file object.
+open(filename,'w') Open a file called filename and use it for writing. This will also return a reference to a file object.
+close filevariable.close() File use is complete.
+'''
+
 fileref = open("d:/code/data/data.txt", "r")
+contents = fileref.read() # read all file
+print(contents)
 ## other code here that refers to variable fileref
 fileref.close()
