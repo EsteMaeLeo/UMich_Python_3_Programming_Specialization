c500b3856a9c488bed46a32ec499dc892a9b5211
EsteMaeLeo <leonel_morales20@hotmail.com>
2026-02-20 19:14:33 -0600

    continue with python

--- course2/moduleCVS.py
+++ course2/moduleCVS.py
@@ -14,14 +14,11 @@ for row_vals in rows[1:]:
 print()
 print("Elegant way with open")
 print()
-with open("../test/reduced_olympics.csv", "r"):
-    fileconnection = open("../test/reduced_olympics.csv", "r")
-    reader = csv.reader(fileconnection)
-    rows = list(reader)
-    headers = rows[0]
-
+with open("../test/reduced_olympics.csv", "r") as f:
+    reader = csv.reader(f)
+    headers = next(reader)
     print(headers)
     #rows[1:] avoid the first column
-    for row_vals in rows[1:]:
+    for row_vals in reader:
         if row_vals[0] != "NA":
             print(f"{row_vals[0]} : {row_vals[1]} : {row_vals[2]}")
