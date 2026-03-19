a0d952bb11df3524ea9a1ab2fdae83bf0af757b4
EsteMaeLeo <leonel_morales20@hotmail.com>
2026-03-18 23:31:48 -0500

    new changes

--- course2/dictonary.py
+++ course2/dictonary.py
@@ -47,3 +47,17 @@ print("Transform a list of keys: ", keys)
 
 for key in inventory.keys():
     print(key, "has the value ", inventory[key])
+
+inventory = {'apples': 430, 'bananas': 312, 'oranges': 525, 'pears': 217}
+
+print(list(inventory.values()))
+
+for v in inventory.values():
+    print("Got", v)
+
+inventory = {'apples': 430, 'bananas': 312, 'oranges': 525, 'pears': 217}
+
+print(list(inventory.items()))
+
+for k, v in inventory.items():
+    print("Got", k, "that maps to", v)    
