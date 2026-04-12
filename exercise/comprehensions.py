9d6746f306cf9fdd48762eef7615ac3be0792db9
EsteMaeLeo <leonel_morales20@hotmail.com>
2026-04-12 00:02:09 -0500

    new code

--- exercise/comprehensions.py
+++ exercise/comprehensions.py
@@ -2,9 +2,11 @@
 # new_list = [expression for item in list if condition]
 # new_list = [expression if/else for item in list ]
 
-
 doubles = []  
 for x in range(1,11):
     doubles.append(x * 2)
     
-print(doubles)
+print("Using loop: ", doubles)
+
+doubles = [x * 2 for x in range(1,11)]
+print("Using comprehension: ", doubles)
