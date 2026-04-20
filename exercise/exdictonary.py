0aecf9bebb8b0eca10258e876a9df8bece0af80c
EsteMaeLeo <leonel_morales20@hotmail.com>
2026-04-19 23:57:40 -0500

    small steps

--- exercise/exdictonary.py
+++ exercise/exdictonary.py
@@ -3,3 +3,17 @@ student = {
     "William" : {"Programing" : 80, "English": 100},
     "John" : {"Programing" : 75, "English": 75}
 }
+
+print(student["David"]["English"])
+
+student["Lex"] = {"Programing" : 80, "English": 100}
+print("adding Lex... ", student)
+
+print("key: ", student.keys())
+print("values:  ", student.values())
+print("Items: ", student.items())
+print("to remove all use student.cler()")
+print("Get: ", student.get("David"))
+
+print("Pop: ", student.pop("David"))
+print(student)
